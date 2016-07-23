57593f8087399e5ff495c5863e11ff3a76a4625b
David Martin <dwmartin41@gmail.com>
2016-07-23 19:15:33 +0100

    added connection status to console card

--- src/clj/phonsole_client/components/console.cljs
+++ src/clj/phonsole_client/components/console.cljs
@@ -1,6 +1,7 @@
 (ns phonsole-client.components.console
   (:require [reagent.dom :as rdom]
-            [re-frame.core :refer [dispatch]]))
+            [re-frame.core :refer [dispatch]]
+            [phonsole-client.components.connection-status :refer [connection-status]]))
 
 (defn console-output [output]
   [:div {:class "output"}
@@ -15,7 +16,9 @@
    [:div {:class "card"}
     (console-output (:output client))
     [:div {:class "card-content"}
-     [:p {:class "card-title"} (:client-id client)]]
+     [:p {:class "card-title"}
+      [:span {:class "margin-horizontal"} (:client-id client)]
+      (connection-status (not (:disconnected? client)))]]
     [:div {:class "card-action"}
      (when (:disconnected? client)
        [:button {:class "btn btn-flat waves-purple"
