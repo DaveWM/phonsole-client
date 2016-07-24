8594872d50c97bcac73071146e2d6ef23bf3210f
David Martin <dwmartin41@gmail.com>
2016-07-24 18:45:05 +0100

    Added support for colours/styling in output. Needs tidying up at some point

--- src/clj/phonsole_client/components/console.cljs
+++ src/clj/phonsole_client/components/console.cljs
@@ -1,14 +1,65 @@
 (ns phonsole-client.components.console
   (:require [reagent.core :as r]
             [re-frame.core :refer [dispatch]]
-            [phonsole-client.components.connection-status :refer [connection-status]]))
+            [phonsole-client.components.connection-status :refer [connection-status]]
+            [clojure.string :as str]))
+
+(defn colour [colour]
+  {:style {:color colour}})
+
+(defn background [colour]
+  {:style {:background-color colour}})
+
+(defn convert-index-map [to-convert transform starting-index]
+  (->> to-convert
+       (map
+        (fn [[offset item-vec]]
+          (map-indexed (fn [i item] [(+ i starting-index offset) (transform item)]) item-vec)))
+       (into {})))
+
+(def ansi-codes
+  (let [colours {0 ["black" "red" "green" "yellow" "blue" "darkmagenta" "cyan" "lightgrey"]
+                 60 ["darkgrey" "lightpink" "lightgreen" "lightyellow" "lightblue" "magenta" "lightcyan" "white"]}]
+    (merge {
+            1 {:style {:font-weight "bold"}}
+            2 {:style {:opacity 0.5}}
+            4 {:style {:text-decoration "underline"}}
+            8 {:style {:visibility "hidden"}}
+            0 {:style {:font-weight "normal" :opacity 1 :text-decoration "none" :visibility "visible"}
+               :class "default-colour default-background"}
+            21 {:style {:font-weight "normal"}}
+            22 {:style {:opacity 1}}
+            24 {:style {:text-decoration "none"}}
+            28 {:style {:visibility "visible"}}
+
+            39 {:class "default-colour"}
+            49 {:class "default-background"}
+            }
+           (convert-index-map colours colour 30)
+           (convert-index-map colours background 40))))
+
+(defn line-parts-to-hiccup [[first-part & other-parts :as parts]]
+  (if (empty? parts)
+    nil
+    (->> (if-let [[match group] (re-matches #"\[(\d*)m" first-part)]
+           (let [number (if (empty? group)
+                          0
+                          (js/parseInt group))]
+             [:span (or (get ansi-codes number) {}) (line-parts-to-hiccup other-parts)])
+           [:span first-part (line-parts-to-hiccup other-parts)])
+         (remove nil?)
+         vec)))
+
+(defn line-to-hiccup [line]
+  (line-parts-to-hiccup (->> (str/split line #"(\[\d*m)")
+                             (remove empty?))))
 
 (defn console-output [output]
   [:div {:class "output"}
    (-> (vec output)
        (conj ">_")
        (->>
-        (map #(identity [:p {:class "line"} %]))))])
+        (map #(identity [:p {:class "line"} (line-to-hiccup %)]))))])
 
 (defn console [client]
   (let [tailing (atom false)]
