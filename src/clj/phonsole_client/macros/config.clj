bd94bbf13c856b43d420f7647602d875c59157ff
David Martin <dwmartin41@gmail.com>
2016-09-20 18:45:45 +0100

    remove useless macro

--- src/clj/phonsole_client/macros/config.clj
+++ src/clj/phonsole_client/macros/config.clj
@@ -1,12 +1,12 @@
 (ns phonsole-client.macros.config
-  (:require [phonsole-client.macros.env-vars :refer [env-var]]))
+  (:require [environ.core :refer [env]]))
 
 (def defaults
   {:server-url "localhost:8080"})
 
 
 (defmacro config [key]
-  (let [value (or (env-var key)
+  (let [value (or (env key)
                   (defaults key))]
     (if (nil? value)
       (throw (Exception. (str "No config var found for key" (name key))))
--- src/clj/phonsole_client/macros/env_vars.clj
+++
@@ -1,6 +0,0 @@
-(ns phonsole-client.macros.env-vars
-  (require [environ.core :refer [env]]))
-
-
-(defmacro env-var [var-name]
-  (env var-name))
